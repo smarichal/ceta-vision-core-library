492bc0f728c0941eab6508081aa31d86cc3af88d
smarichal <sebastian_marichal@hotmail.com>
2017-10-06 16:35:51 +0200

    show debug

--- core/src/main/java/edu/ceta/vision/core/topcode/TopCodeDetectorDesktop.java
+++ core/src/main/java/edu/ceta/vision/core/topcode/TopCodeDetectorDesktop.java
@@ -15,23 +15,30 @@ import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
 
 import edu.ceta.vision.core.blocks.Block;
+import edu.ceta.vision.core.utils.ShowImage;
 
 public class TopCodeDetectorDesktop extends TopCodeDetector{
 
 	private BufferedImage image;
 	
-	private boolean busy;
+	private boolean busy, showdebug;
 	private Object syncObject = new Object();
 	
 	private Mat frame;
 	private VideoCapture capture;
+
+	private ShowImage showImagePanel;
 	
 	public TopCodeDetectorDesktop(int max_markers, boolean probMode,int max_marker_diameter, 
 				int size_cache, boolean cacheEnabled,boolean allow_different_spot_distance,
-				boolean multiple_markers_per_block, Rect detectionZone, boolean captureFrames) {
+				boolean multiple_markers_per_block, Rect detectionZone, boolean captureFrames, boolean showDebugFrames) {
 		super(max_markers, probMode, size_cache, cacheEnabled, 
 			allow_different_spot_distance, multiple_markers_per_block, detectionZone);
 		this.busy=false;
+		this.showdebug = showDebugFrames;
+		if(this.showdebug){
+			this.showImagePanel = new ShowImage();
+		}
 		this.scanner = new ScannerDesktop();
 		if(max_marker_diameter>0){
 			this.scanner.setMaxCodeDiameter(max_marker_diameter);
@@ -64,13 +71,21 @@ public class TopCodeDetectorDesktop extends TopCodeDetector{
 		this.setBusy(true);
 		if(capture!=null && capture.isOpened()){
 			capture.read(frame);
-			Core.flip(frame, frame, 0);
+			//Core.flip(frame, frame, 0);
+			//Core.flip(frame, frame, 1);
+			Core.flip(frame, frame, -1);
 			BufferedImage rgbaImage = new BufferedImage((int)capture.get(Highgui.CV_CAP_PROP_FRAME_WIDTH), (int)capture.get(Highgui.CV_CAP_PROP_FRAME_HEIGHT), BufferedImage.TYPE_3BYTE_BGR);
 			
-			this.image=rgbaImage.getSubimage(this.detectionZone.x, this.detectionZone.y, this.detectionZone.width-this.detectionZone.x, this.detectionZone.height-this.detectionZone.x);			
+			//this.image=rgbaImage.getSubimage(this.detectionZone.x, this.detectionZone.y, this.detectionZone.width-this.detectionZone.x, this.detectionZone.height-this.detectionZone.x);
+			this.image=rgbaImage;
 			byte[] data = ((DataBufferByte)this.image.getRaster().getDataBuffer()).getData();
 			frame.get(0,0, data);
 
+			if(this.showdebug){
+				this.showImagePanel.setImage(image);
+				//this.showImagePanel.repaint();
+				this.showImagePanel.paint();
+			}
 			this.markers = ((ScannerDesktop)this.scanner).scan(this.image);
 			System.out.println("##### Marcadores detectados = " + this.markers.size());
 			
---
+++ core/src/main/java/edu/ceta/vision/core/utils/ShowImage.java
@@ -0,0 +1,61 @@
+package edu.ceta.vision.core.utils;
+import java.awt.Graphics;
+import java.awt.Panel;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+
+public class ShowImage extends Panel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	BufferedImage  image;
+	private Graphics graphics;
+	
+	
+	public ShowImage(){
+		JFrame frame = new JFrame("Display image");
+		frame.getContentPane().add(this);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+	}
+	
+//	public ShowImage() {
+//		try {
+//			System.out.println("Enter image name\n");
+//			BufferedReader bf=new BufferedReader(new 
+//					InputStreamReader(System.in));
+//			String imageName=bf.readLine();
+//			File input = new File(imageName);
+//			image = ImageIO.read(input);
+//		} catch (IOException ie) {
+//			System.out.println("Error:"+ie.getMessage());
+//		}
+//	}
+
+	public void setImage(BufferedImage image){
+		this.image = image;
+	}
+	
+	public void paint(){
+		this.repaint();
+	}
+	public void paint(Graphics g) {
+		if(this.graphics ==null){
+			this.graphics = g; 
+		}
+		System.out.println("painting--------");
+		g.drawImage( image, 0, 0, null);
+	}
+
+//	static public void main(String args[]) throws
+//	Exception {
+//		JFrame frame = new JFrame("Display image");
+//		Panel panel = new ShowImage();
+//		frame.getContentPane().add(panel);
+//		frame.setSize(500, 500);
+//		frame.setVisible(true);
+//	}
+}
